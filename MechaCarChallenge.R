054b1c7549e0ef0e59ea36ca0a20743d77e15dc7
ian-zukowski <104320798+ian-zukowski@users.noreply.github.com>
2022-08-15 13:15:26 -0400

    Deliverable 3 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,6 +14,20 @@ summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,
 
 #D2: Gather summary stastics of suspension coil PSI column
 total_summary <- msusp %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+total_summary
 #D2: Create lot summary using group_by on 'Manufacturing Lot' column
 lot_summary <- msusp %>% group_by(msusp$Manufacturing_Lot)%>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary
+
+
+#D3: Filter dataset for only lot1, lot2, and lot3 vehicles
+lot1 <- msusp %>% filter(msusp$Manufacturing_Lot=='Lot1')
+lot2 <- msusp %>% filter(msusp$Manufacturing_Lot=='Lot2')
+lot3 <- msusp %>% filter(msusp$Manufacturing_Lot=='Lot3')
+#D3: t-test on PSI amongst all cars
+t.test(msusp$PSI,mu=1500)
+#D3: t-test on PSI separated by lot
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+
